d586560e366026319ee45b61acea0de0f05c18f0
petmik1 <45992050+petmik1@users.noreply.github.com>
2021-12-12 23:32:26 +0100

    added event listener to the product page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+const btn = document.querySelector(".btn_addToCart");
+const hiddenBox = document.querySelector(".hiddenInfo");
+
+btn.addEventListener("click", displayBox);
+
+function displayBox() {
+    hiddenBox.style.display = "flex";
+}
