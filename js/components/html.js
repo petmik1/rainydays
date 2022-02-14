46c42edc494a664d07100010927348bd853691fc
petmik1 <petter.mikalsen.pehm@gmail.com>
2022-02-15 00:46:40 +0100

    tried to get qoury stings to work

--- js/components/html.js
+++ js/components/html.js
@@ -1,10 +1,9 @@
 function createHTML (product, gender){
-
     product.forEach(function (product){
         if(product.categories[0].slug === gender){
         listContainer.innerHTML += `
             <div class="product">
-                <a href="product.html">
+                <a href="../pages/product.html" class="productItem" onclick="addSearchParms(${product.id})">
                     <img src="${product.images[0].thumbnail}" />
                     <h2>${product.name}</h2>
                     <p>${product.prices.price} ${product.prices.currency_symbol}</p>
--- js/women.js
+++ js/women.js
@@ -1,12 +1,14 @@
 const url = "http://rainydaysapi.petter-mikalsen.no/wp-json/wc/store/products";
 const listContainer = document.querySelector(".listContainer");
 const gender = "women";
+
 async function getProducts() {
     try{
         
         const response = await fetch(url);
         const result = await response.json();
         createHTML(result, gender);
+        
     }
     catch(error){
         console.log(error);
@@ -15,3 +17,9 @@ async function getProducts() {
 }
 getProducts();
 
+
+let queryStr = "id=1"
+let searchParams = new URLSearchParams(queryStr);
+function addSearchParms(id){
+    searchParams.set("id", id)
+}
