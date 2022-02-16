55553df13e0810c33c4f6a26f5e1239dfe734741
petmik1 <petter.mikalsen.pehm@gmail.com>
2022-02-16 23:08:53 +0100

    https product.js

--- js/product.js
+++ js/product.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector(".btn_addToCart");
 const hiddenBox = document.querySelector(".hiddenInfo");
 const productMain = document.querySelector(".product-main");
-const url = "http://rainydaysapi.petter-mikalsen.no/wp-json/wc/store/products";
+const url = "https://rainydaysapi.petter-mikalsen.no/wp-json/wc/store/products";
 let params = new URLSearchParams(document.location.search);
 let id = parseInt(params.get("id"));
 
